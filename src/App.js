b2135dd753369bbd4cd1883d8be89702313f1d80
mim-mel <mfblade2014@gmail.com>
2022-10-10 12:11:04 +0900

    feat: react-router 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import MainPage from './pages/mainpage/Mainpage';
+import BlogPage from './pages/blogpage/BlogPage';
 
 function App() {
   return (
-    <div className='App'>
-      <MainPage />
-    </div>
+    <BrowserRouter basename='become-a-pro-composer'>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/blog' element={<BlogPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/blogpage/BlogPage.jsx
@@ -0,0 +1,5 @@
+const BlogPage = () => {
+  return <div>블로그 페이지</div>;
+};
+
+export default BlogPage;
