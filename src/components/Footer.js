ac3c812e0ae8ca86456be0c552338a550294fd19
mim-mel <mfblade2014@gmail.com>
2023-03-12 23:31:19 +0900

    feat: add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,50 @@ const Footer = () => {
         </ButtonWrap>
         <MiniText>Instant access. No sign-up needed.</MiniText>
       </Footer1Wrap>
-      <Footer2Wrap></Footer2Wrap>
+      <Footer2Wrap>
+        <Line />
+        <TextWrap>
+          <TextBlock>
+            <Footer2Title>ABOUT</Footer2Title>
+            <Footer2Text>Company</Footer2Text>
+            <Footer2Text>Terms</Footer2Text>
+            <Footer2Text>Privacy Policy</Footer2Text>
+            <Footer2Text>Contact</Footer2Text>
+            <Footer2Text>Help</Footer2Text>
+          </TextBlock>
+          <TextBlock>
+            <Footer2Title>LEARN</Footer2Title>
+            <Footer2Text>Beginner’s Guide</Footer2Text>
+            <Footer2Text>Designers</Footer2Text>
+            <Footer2Text>Classrooms</Footer2Text>
+            <Footer2Text>Newcomers</Footer2Text>
+          </TextBlock>
+          <TextBlock>
+            <Footer2Title>PRACTISE</Footer2Title>
+            <Footer2Text>Composer Exercises</Footer2Text>
+            <Footer2Text>Manuals</Footer2Text>
+            <Footer2Text>Tutorials</Footer2Text>
+            <Footer2Text>Communities</Footer2Text>
+          </TextBlock>
+          <TextBlock>
+            <Footer2Title>SHARE</Footer2Title>
+            <Footer2Text>BAPC Discord</Footer2Text>
+            <Footer2Text>Editorials</Footer2Text>
+            <Footer2Text>Product</Footer2Text>
+            <Footer2Text>Newsroom</Footer2Text>
+          </TextBlock>
+        </TextWrap>
+        <Logo src='image/logo.png' />
+        <RightText>
+          © 2023 Become A Pro Composer. All rights reserved.
+        </RightText>
+        <IconWrap>
+          <SocialIcon src='image/youtube-w.png' />
+          <SocialIcon src='image/discord-w.png' />
+          <SocialIcon src='image/insta-w.png' />
+          <SocialIcon src='image/facebook-w.png' />
+        </IconWrap>
+      </Footer2Wrap>
     </>
   );
 };
@@ -60,12 +103,6 @@ const Title = styled.div`
     font-size: 17px;
     line-height: 27px;
   }
-`;
-
-const Footer2Wrap = styled.div`
-  width: 100%;
-  height: 350px;
-  background-color: #0d1f2d;
 `;
 
 const ButtonWrap = styled.div`
@@ -113,7 +150,7 @@ const Button = styled.button`
   }
 
   img {
-    margin-left: 10px;
+    margin-left: 7px;
   }
 
   @media (max-width: 768px) {
@@ -131,4 +168,69 @@ const MiniText = styled.div`
     font-size: 14px;
   }
 `;
+
+const Footer2Wrap = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: #0d1f2d;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Line = styled.div`
+  width: 858px;
+  height: 0.12px;
+  background-color: #f5f5f5;
+  margin-top: 60px;
+  opacity: 0.2;
+`;
+
+const TextWrap = styled.div`
+  display: flex;
+  width: 858px;
+  justify-content: space-between;
+  margin-top: 54px;
+`;
+
+const TextBlock = styled.div``;
+
+const Footer2Title = styled.div`
+  font-family: 'Exo2-Bold';
+  color: #fe5d26;
+  font-size: 18px;
+  margin-bottom: 40px;
+`;
+
+const Footer2Text = styled.div`
+  font-family: 'Latto-Light';
+  color: #f4f4f4;
+  font-size: 16px;
+  margin-bottom: 13px;
+`;
+
+const Logo = styled.img`
+  margin: 17px 0;
+`;
+
+const RightText = styled.div`
+  font-family: 'Exo2-Light';
+  font-size: 16px;
+  color: #f4f4f4;
+  margin-bottom: 25px;
+`;
+
+const IconWrap = styled.div`
+  width: 140px;
+  height: 30px;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 55px;
+`;
+
+const SocialIcon = styled.img`
+  /* margin-top: ${props => props.marginTop}; */
+`;
+
 export default Footer;
